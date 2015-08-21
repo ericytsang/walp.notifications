3055618a843a40aae43492f127558da227509c58
Eric Tsang <surplus.et@gmail.com>
2015-08-21 10:52:54 -0700

    added buttons, and empty onClick methods for each
    signed-off-by: Eric Tsang <surplus.et@gmail.com>

--- app/src/main/java/com/example/erictsang/notifications/MainActivity.java
+++ app/src/main/java/com/example/erictsang/notifications/MainActivity.java
@@ -2,11 +2,12 @@ package com.example.erictsang.notifications;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity
 {
+
+    // AppCompatActivity
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
@@ -15,28 +16,17 @@ public class MainActivity extends AppCompatActivity
         setContentView(R.layout.activity_main);
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu)
+    // public interface: button onClick methods
+
+    public void insertNotification(View unused)
     {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main,menu);
-        return true;
     }
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item)
+    public void updateNotification(View unused)
     {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
+    }
 
-        //noinspection SimplifiableIfStatement
-        if(id == R.id.action_settings)
-        {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
+    public void deleteNotification(View unused)
+    {
     }
 }
